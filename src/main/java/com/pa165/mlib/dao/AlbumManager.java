3fcb68719c3ce0efab2f04fa0ecbd0f7f770c9ed
Ivo Bek <ibek@redhat.com>
2013-10-12 01:54:58 +0200

    added javadoc comments for Song DAO and Album DAO

--- src/main/java/com/pa165/mlib/dao/AlbumManager.java
+++ src/main/java/com/pa165/mlib/dao/AlbumManager.java
@@ -18,10 +18,19 @@ public class AlbumManager {
     @PersistenceContext(unitName = "mlib-pu")
     EntityManager em;
     
+    /**
+     * Persist the given album to persistence context.
+     * @param album to be persisted
+     */
     public void addAlbum(Album album) {
         em.persist(album);
     }
     
+    /**
+     * Update the given album in persistence context and database after the commit.
+     * @param album to be updated
+     * @return updated album
+     */
     public Album updateAlbum(Album album) {
         if (album == null) {
             return null;
@@ -29,6 +38,10 @@ public class AlbumManager {
         return em.merge(album);
     }
     
+    /**
+     * Remove album from the persistence context and database after the commit.
+     * @param album to be removed
+     */
     public void removeAlbum(Album album) {
         if (album != null && !em.contains(album)) {
             album = em.merge(album);
@@ -36,23 +49,42 @@ public class AlbumManager {
         em.remove(album);
     }
     
+    /**
+     * Get all the albums.
+     * @return all the albums
+     */
     public List<Album> getAll() {
         return em.createQuery("SELECT a FROM Album a")
                 .getResultList();
     }
     
+    /**
+     * Get album with given unique identifier.
+     * @param id unique identifier for album
+     * @return album
+     */
     public Album getAlbum(long id) {
         return (Album) em.createQuery("SELECT a FROM Album a WHERE a.id : id")
                 .setParameter("id", id)
                 .getSingleResult();
     }
     
+    /**
+     * Get all the albums with given title.
+     * @param title what should be albums called
+     * @return albums with defined title
+     */
     public List<Album> getAlbumsWithTitle(String title) {
         return em.createQuery("SELECT a FROM Album a WHERE a.title = :title")
                 .setParameter("title", title)
                 .getResultList();
     }
     
+    /**
+     * Get all the albums with given artist.
+     * @param artist who is author of the required albums
+     * @return albums with defined artist
+     */
     public List<Album> getAlbumsWithArtist(Artist artist) {
         return em.createQuery("select distinct a from Album a inner join a.songs as song inner join  song.artist as artist where artist.id = :artist")
                 .setParameter("artist", artist.getId())
--- src/main/java/com/pa165/mlib/dao/SongManager.java
+++ src/main/java/com/pa165/mlib/dao/SongManager.java
@@ -17,10 +17,19 @@ public class SongManager {
     @PersistenceContext(unitName = "mlib-pu")
     EntityManager em;
     
+    /**
+     * Persist the given song to persistence context.
+     * @param song to be persisted
+     */
     public void addSong(Song song) {
         em.persist(song);
     }
     
+    /**
+     * Update the given song in persistence context and database after the commit.
+     * @param song to be updated
+     * @return updated song
+     */
     public Song updateSong(Song song) {
         if (song == null) {
             return null;
@@ -28,6 +37,10 @@ public class SongManager {
         return em.merge(song);
     }
     
+    /**
+     * Remove song from the persistence context and database after the commit.
+     * @param song to be removed
+     */
     public void removeSong(Song song) {
         if (song != null && !em.contains(song)) {
             song = em.merge(song);
@@ -35,17 +48,31 @@ public class SongManager {
         em.remove(song);
     }
     
+    /**
+     * Get all the songs.
+     * @return all the songs
+     */
     public List<Song> getAll() {
         return em.createQuery("SELECT s FROM Song s")
                 .getResultList();
     }
     
+    /**
+     * Get song with given unique identifier.
+     * @param id unique identifier for song
+     * @return song
+     */
     public Song getSong(long id) {
         return (Song) em.createQuery("SELECT a FROM Album a WHERE a.id : id")
                 .setParameter("id", id)
                 .getSingleResult();
     }
     
+    /**
+     * Get all the songs with given title.
+     * @param title what should be song called
+     * @return songs with defined title
+     */
     public List<Song> getSongsWithTitle(String title) {
         return em.createQuery("SELECT s FROM Song s WHERE s.title = :title")
                 .setParameter("title", title)
