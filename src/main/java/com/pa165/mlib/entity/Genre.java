b387a17c926b959276434f2f0e47609ef660cb0b
Ivo Bek <ibek@redhat.com>
2013-10-11 20:59:04 +0200

    added song and genre

---
+++ src/main/java/com/pa165/mlib/entity/Genre.java
@@ -0,0 +1,78 @@
+package com.pa165.mlib.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author ibek
+ */
+@Entity
+public class Genre implements Serializable {
+    
+    @Id
+    private String name;
+    
+    @OneToMany(mappedBy = "genre")
+    private List<Song> songs;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Genre other = (Genre) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the songs
+     */
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    /**
+     * @param songs the songs to set
+     */
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
+
+    @Override
+    public String toString() {
+        return "Genre{ name=" + name + "}";
+    }
+    
+}
---
+++ src/main/java/com/pa165/mlib/entity/Song.java
@@ -0,0 +1,147 @@
+package com.pa165.mlib.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author ibek
+ */
+@Entity
+public class Song implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String title;
+    
+    private int bitrate;
+    
+    private int position;
+    
+    private String commentary;
+    
+    @ManyToOne
+    private Genre genre;
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Song other = (Song) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the bitrate
+     */
+    public int getBitrate() {
+        return bitrate;
+    }
+
+    /**
+     * @param bitrate the bitrate to set
+     */
+    public void setBitrate(int bitrate) {
+        this.bitrate = bitrate;
+    }
+
+    /**
+     * @return the position
+     */
+    public int getPosition() {
+        return position;
+    }
+
+    /**
+     * @param position the position to set
+     */
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    /**
+     * @return the commentary
+     */
+    public String getCommentary() {
+        return commentary;
+    }
+
+    /**
+     * @param commentary the commentary to set
+     */
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
+    /**
+     * @return the genre
+     */
+    public Genre getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{ id=" + id + 
+                ", title=" + title + 
+                ", bitrate=" + bitrate + 
+                ", position=" + position + 
+                ", commentary=" + commentary + "}";
+    }
+    
+}
