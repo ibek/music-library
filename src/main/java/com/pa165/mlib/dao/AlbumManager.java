9d146e66d383e14a9380263a4ca6d5c18d0e9154
Ivo Bek <ibek@redhat.com>
2013-10-11 21:18:39 +0200

    example how to inject entity manager

--- src/main/java/com/pa165/mlib/dao/AlbumManager.java
+++ src/main/java/com/pa165/mlib/dao/AlbumManager.java
@@ -1,11 +1,15 @@
 package com.pa165.mlib.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  *
  * @author ibek
  */
 public class AlbumManager {
     
-    
+    @PersistenceContext(unitName = "mlib-pu")
+    EntityManager em;
     
 }
