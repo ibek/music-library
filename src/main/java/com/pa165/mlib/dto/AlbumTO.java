e24fee25b83d47c5843f00f0949e013b43078af6
tomparys <tom.parys@gmail.com>
2013-10-28 14:29:43 +0100

    Added AlbumTO, and a skeleton of SongTO and ArtistTO due to dependencies.

---
+++ src/main/java/com/pa165/mlib/dto/AlbumTO.java
@@ -0,0 +1,44 @@
+package com.pa165.mlib.dto;
+
+import com.pa165.mlib.entity.Song;
+import java.util.List;
+
+/**
+ * Album Transfer Object
+ * 
+ * @author tomparys
+ */
+public class AlbumTO {
+    
+    private String title;
+    
+    private String released;
+    
+    private List<SongTO> songs;
+    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    public List<SongTO> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<SongTO> songs) {
+        this.songs = songs;
+    }
+
+}
---
+++ src/main/java/com/pa165/mlib/dto/SongTO.java
@@ -0,0 +1,15 @@
+package com.pa165.mlib.dto;
+
+import com.pa165.mlib.entity.Song;
+import java.util.List;
+
+/**
+ * Song Transfer Object
+ * 
+ * @author                        ????? - this is just mock object so I can use it in my AlbumTO,
+ *                                          someone please finish it
+ */
+public class SongTO {
+    
+    
+}
