ddb17506588072148637bebaff71191e717383b2
brazdil jan <323181@mail.muni.cz>
2013-10-29 19:42:42 +0100

    Created Song transfer object - SongTO class.

--- src/main/java/com/pa165/mlib/dto/SongTO.java
+++ src/main/java/com/pa165/mlib/dto/SongTO.java
@@ -1,15 +1,79 @@
 package com.pa165.mlib.dto;
 
-import com.pa165.mlib.entity.Song;
-import java.util.List;
+import com.pa165.mlib.entity.Album;
+import com.pa165.mlib.entity.Artist;
+import com.pa165.mlib.entity.Genre;
 
 /**
  * Song Transfer Object
  * 
- * @author                        ????? - this is just mock object so I can use it in my AlbumTO,
- *                                          someone please finish it
+ * Zenian
  */
 public class SongTO {
+    
+    private String title;    
+    private Integer bitrate;
+    private Integer position;
+    private String commentary;
+    private Genre genre;
+    private Album album;
+    private Artist artist;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getBitrate() {
+        return bitrate;
+    }
+
+    public void setBitrate(Integer bitrate) {
+        this.bitrate = bitrate;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public String getCommentary() {
+        return commentary;
+    }
+
+    public void setCommentary(String commentary) {
+        this.commentary = commentary;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public Album getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(Album album) {
+        this.album = album;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
     
     
 }
