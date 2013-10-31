9523cfdc77892996ba83e5c6f40af5c1e7a9747f
brazdil jan <323181@mail.muni.cz>
2013-10-31 10:04:25 +0100

    added comments to SongTO class

--- src/main/java/com/pa165/mlib/dto/SongTO.java
+++ src/main/java/com/pa165/mlib/dto/SongTO.java
@@ -19,58 +19,100 @@ public class SongTO {
     private Album album;
     private Artist artist;
 
+    /**
+     * @return the title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * @param title the title to set
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * @return the bitrate
+     */
     public Integer getBitrate() {
         return bitrate;
     }
 
+    /**
+     * @param bitrate the bitrate to set
+     */
     public void setBitrate(Integer bitrate) {
         this.bitrate = bitrate;
     }
 
+    /**
+     * @return the position
+     */
     public Integer getPosition() {
         return position;
     }
 
+    /**
+     * @param position the position to set
+     */
     public void setPosition(Integer position) {
         this.position = position;
     }
 
+    /**
+     * @return the commentary
+     */
     public String getCommentary() {
         return commentary;
     }
 
+    /**
+     * @param commentary the commentary to set
+     */
     public void setCommentary(String commentary) {
         this.commentary = commentary;
     }
 
+    /**
+     * @return the genre
+     */
     public Genre getGenre() {
         return genre;
     }
 
+    /**
+     * @param genre the genre to set
+     */
     public void setGenre(Genre genre) {
         this.genre = genre;
     }
 
+    /**
+     * @return the album
+     */
     public Album getAlbum() {
         return album;
     }
 
+    /**
+     * @param album the album to set
+     */
     public void setAlbum(Album album) {
         this.album = album;
     }
 
+    /**
+     * @return the artist
+     */
     public Artist getArtist() {
         return artist;
     }
 
+    /**
+     * @param artist the artist to set
+     */
     public void setArtist(Artist artist) {
         this.artist = artist;
     }
