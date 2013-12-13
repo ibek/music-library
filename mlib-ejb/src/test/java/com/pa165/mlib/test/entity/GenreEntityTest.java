b8f32b3e3bdbecd61c13fc08db1e5b48e69dee62
tomparys <tom.parys@gmail.com>
2013-12-13 23:59:32 +0100

    Test polish.

--- mlib-ejb/src/test/java/com/pa165/mlib/test/entity/GenreEntityTest.java
+++ mlib-ejb/src/test/java/com/pa165/mlib/test/entity/GenreEntityTest.java
@@ -1,8 +1,10 @@
 package com.pa165.mlib.test.entity;
 
 import com.pa165.mlib.dao.impl.GenreDaoImpl;
+import com.pa165.mlib.entity.Artist;
 import com.pa165.mlib.entity.Genre;
 import com.pa165.mlib.test.EntityTestBase;
+import java.util.List;
 import javax.persistence.EntityManager;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -33,14 +35,19 @@ public class GenreEntityTest extends EntityTestBase {
     public void testGetAll() throws Throwable {
         Genre rock = new Genre();
         rock.setName("rock3");
+        Genre rock2 = new Genre();
+        rock2.setName("rock31");
         GenreDaoImpl gm = new GenreDaoImpl();
         EntityManager em = getTestEntityManager();
         gm.setEntityManager(em);
         em.getTransaction().begin();
         gm.addGenre(rock);
+        gm.addGenre(rock2);
         em.getTransaction().commit();
-        Genre rock2 = gm.getGenre("rock3");
-        assertEquals(rock, rock2);
+        List<Genre> list = gm.getAll();
+        assertTrue(list != null);
+        assertTrue(list.contains(rock));
+        assertTrue(list.contains(rock2));
     }
     
     @Test
