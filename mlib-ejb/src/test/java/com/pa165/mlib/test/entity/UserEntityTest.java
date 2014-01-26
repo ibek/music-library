5625240f75720b3e499fad70fd7a4c16c0c1cc9a
Ivo Bek <ibek@redhat.com>
2014-01-26 01:38:54 +0100

    fixed the UserEntityTest

--- mlib-ejb/src/test/java/com/pa165/mlib/test/entity/UserEntityTest.java
+++ mlib-ejb/src/test/java/com/pa165/mlib/test/entity/UserEntityTest.java
@@ -21,7 +21,7 @@ public class UserEntityTest extends EntityTestBase {
         
         User peter = new User();
         peter.setUsername("peterParker");
-        peter.setPasswordHash("asdffdsaasdffdsaasdffdsaasdffdsaasdffdsa");
+        peter.setPasswordHash("04f8996da763b7a969b1028ee3007569eaf3a635486ddab211d512c85b9df8fb");
         
         em.getTransaction().begin();
         ud.addUser(peter);
